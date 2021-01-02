d8adb357589cb79e60ce88d04af0c053ec253f8c
Elbert <etse168@gmail.com>
2021-01-02 18:42:35 -0500

    added intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,13 @@
+const superheroes = require('superheroes');
+
+let mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+
+// needed to run npm init to create package.json then npm install whatever
+// package you want
+
+const supervillains = require('supervillains');
+
+console.log(supervillains.random());
