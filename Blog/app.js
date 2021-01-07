d2277ec027b72d8f79fdf1485732d3d6506b791e
Elbert <etse168@gmail.com>
2021-01-06 20:49:07 -0500

    on challenge 20 for blog

--- Blog/app.js
+++ Blog/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,47 +11,69 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const posts = [];
 
 // |-----Root route-----|
 
-app.get("/", function(req, res){
-  res.render("home", {homeText: homeStartingContent});
-});
+app.get("/", function (req, res) {
+  res.render("home", {
+    homeText: homeStartingContent,
+    posts: posts
+  });
 
+});
 
 
 // |-----About route-----|
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutText: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutText: aboutContent });
 });
 
 
 
 // |-----Contact route-----|
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactText: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactText: contactContent });
 });
 
 // |-----Compose route-----|
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const post = {
     title: req.body.title,
     text: req.body.postText,
   };
+
+  posts.push(post);
+  res.redirect("/"); // SHOULD REDIRECT AND THEN DISPLAY ALL OF THE POSTS
+});
+
+
+// |-----posts route-----|
+
+app.get("/posts/:title", function(req, res){
+  const target = _.lowerCase(req.params.title);
   
-  console.log(post.title + " " + post.text);
-})
+  posts.forEach(function(post){
 
+    if(_.lowerCase(post.title) === target)
+      res.render("post", {
+        title: post.title,
+        text: post.text,
+      });
+  });
 
-app.listen(3000, function() {
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
